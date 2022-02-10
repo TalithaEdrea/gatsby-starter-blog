0c68bba1a07aa10c7e11c1404f454f245ea74abe
TalithaEdrea <99401734+TalithaEdrea@users.noreply.github.com>
2022-02-10 20:42:45 +0800

    content

--- src/pages/new-page.jsx
+++ src/pages/new-page.jsx
@@ -5,11 +5,5 @@ export default function Component() {
 }
 # Talitha Edrea Journey
 ## _Three Years After Leaving Indonesia_
-
-[![N|Solid](https://cldup.com/dTxpPi9lDf.thumb.png)](https://nodesource.com/products/nsolid)
-
-[![Build Status](https://travis-ci.org/joemccann/dillinger.svg?branch=master)](https://travis-ci.org/joemccann/dillinger)
-
-Hello! This is my third year I stay in Taiwan. A lot of things happens during past year. 
-
+ 
 This is a [link](https://talithaedrea.wordpress.com)
